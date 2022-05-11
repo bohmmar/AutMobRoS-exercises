be5194b96a2371a65f9c147d9f807f920a628fb2
bohmmar <bohmmar@students.zhaw.ch>
2022-05-11 20:46:19 +0200

    Safety System implemented

---
+++ build/CMakeFiles/3.16.5/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.10.16.3-microsoft-standard-WSL2")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.10.16.3-microsoft-standard-WSL2")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/opt/ost-devel/1.0/sysroots/x86_64-pokysdk-linux/usr/share/cmake/OEToolchainConfig.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "cortexa8hf-neon-poky")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
